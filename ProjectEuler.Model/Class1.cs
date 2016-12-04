eb5f2c078987b26ee5a264aad1b747055cae686b
Nollpeter <noll.peter@hotmail.com>
2016-12-04 22:57:06 +0100

    Initial Commit

---
+++ ProjectEuler.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProjectEuler.Model
+{
+	public class Class1
+	{
+	}
+}
---
+++ ProjectEuler.Test/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ProjectEuler.Test
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void Test1()
+		{
+
+			Assert.True(true);
+		}
+	}
+}
